88d45c71771d13469670d9b59c557ba1c0cdbd7d
Jack Connolly <jconnolly226@gmail.com>
2019-01-21 21:51:55 -0500

    2.10 completed

--- youAreAwesome2/ViewController.swift
+++ youAreAwesome2/ViewController.swift
@@ -11,25 +11,44 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
-    
+    var index = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
     }
+    
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        let message1 = "You are Awesome!!"
-        let message2 = "You are Great!!"
-        let message3 = "You are Amazing!!"
-        if messageLabel.text == message1 {
-            messageLabel.text = message2
-        }else if messageLabel.text == message2{
-            messageLabel.text = message3
-        }else {
-            messageLabel.text = message1
-        
+        let messages = ["You are Awesome!",
+                        "You are Great!",
+                        "You are Fantastic!",
+                        "When the Genius Bar needs help, they call you",
+                        "You brighten my day!",
+                        "You are da bomb!",
+                        "Hey, fabulous!",
+                        "You are tremendous!",
+                        "You've got the design skills of Jony Ive!",
+                        "I can't wait to download your app!"]
+        messageLabel.text = messages[index]
+        if index == messages.count - 1{
+            index = 0
+        }else{
+            index += 1
         }
+        
+//        let message1 = "You are Awesome!!"
+//        let message2 = "You are Great!!"
+//        let message3 = "You are Amazing!!"
+//        if messageLabel.text == message1 {
+//            messageLabel.text = message2
+//        }else if messageLabel.text == message2{
+//            messageLabel.text = message3
+//        }else {
+//            messageLabel.text = message1
+//
+//        }
+        
     }
     
     
