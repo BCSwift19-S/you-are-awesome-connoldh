75b4f1dddcb116d069c13013e341ca59036c2a63
Jack Connolly <jconnolly226@gmail.com>
2019-01-27 18:47:36 -0500

    2.14 completed - using declare but not initialized

--- youAreAwesome2/ViewController.swift
+++ youAreAwesome2/ViewController.swift
@@ -30,7 +30,8 @@ class ViewController: UIViewController {
                         "You are tremendous!",
                         "You've got the design skills of Jony Ive!",
                         "I can't wait to download your app!"]
-        var newIndex = -1
+//        var newIndex = -1
+        var newIndex: Int
         
         repeat {
             newIndex = Int.random(in: 0..<messages.count)
